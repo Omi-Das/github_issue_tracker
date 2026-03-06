69abb4322a3d3f1a98a4e11cc99d01fa57268742
Omi-Das <omi06133@gmail.com>
2026-03-06 21:17:00 +0600

    Login HTML and JavaScript added

---
+++ login.js
@@ -0,0 +1,25 @@
+console.log("hi");
+
+// Login Button Element-ti dhorun
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", function(event) {
+    // Form reload bondho korar jonno
+    event.preventDefault();
+
+    // Input field gulo theke value gulo nile
+    const userName = document.getElementById("input-username").value;
+    const pin = document.getElementById("input-password").value;
+
+    // Validation Check (Shothik Number o Pin kina)
+    const validUserName = "admin";
+    const validPin = "admin123";
+
+    if (userName === validUserName && pin === validPin) {
+        alert("Login successful!");
+        // Home page-e niye jaoa hosche
+        window.location.href = "home.html"; 
+    } else {
+        alert("Vul Number athoba Pin diyechen. Abar chesta korun!");
+    }
+});
